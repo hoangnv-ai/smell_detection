ef0c421b2a07b24fc0eb308dffa8bd752e726337
hoangnv-ai <hoangnv.ai@gmail.com>
2026-06-15 14:49:26 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,6 @@
+# #!/bin/bash
+
+SMELL_NAME=$1
 cd /workspace/smell_detection
-python train/prompt_tuning.py --train_file /workspace/DeepLearningSmells/dataset/ComplexMethod/train.csv --valid_file /workspace/DeepLearningSmells/dataset/ComplexMethod/val.csv
+python train/prompt_tuning.py --train_file /workspace/smell_detection/dataset/${SMELL_NAME}/train.csv --valid_file /workspace/smell_detection/dataset/${SMELL_NAME}/val.csv --model_name_or_path "deepseek-ai/deepseek-coder-1.3b-base"
+
