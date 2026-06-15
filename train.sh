cd0b2487484b0eb35f0faa82bdfe9051b77129cd
hoangnv-ai <hoangnv.ai@gmail.com>
2026-06-15 13:49:49 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 cd /workspace/smell_detection
-python train/prompt_tuning.py
+python train/prompt_tuning.py --train_file /workspace/DeepLearningSmells/dataset/ComplexMethod/train.csv --valid_file /workspace/DeepLearningSmells/dataset/ComplexMethod/val.csv
